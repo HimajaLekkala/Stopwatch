58123f72843c0fd9cb72c689b27c4877007d0117
himaja <himajalekkala@himajas-MacBook-Pro.local>
2024-08-31 12:32:42 +0530

    Initial commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by himaja on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
